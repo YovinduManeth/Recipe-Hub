87e31d9fcd5db8138c33a7003daeb43cb8b33378
Yovindu Maneth <yovindumaneth842@gmail.com>
2026-08-05 23:43:51 +0530

    Updated database backup

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 05, 2026 at 07:58 PM
+-- Generation Time: Aug 05, 2026 at 08:12 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
